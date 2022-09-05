c4c7bb7c3f6dea273d17df2c1a260f1560c1d9f0
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-09-05 10:48:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     st.markdown("""----""")
     st.title("📊 Diagramme")
     try:
-        fig = px.histogram(df, x="is_legendary", color="is_legendary",title="Verteilung der Klasse is_legendary (Legendäre Pokemons))
+        fig = px.histogram(df, x="is_legendary", color="is_legendary",title="Verteilung der Klasse is_legendary (Legendäre Pokemons)")
         fig.update_layout(xaxis_range=[0,1,1])
         fig.update_layout(yaxis_title="Anzahl")
         st.plotly_chart(fig, use_container_width=True)
